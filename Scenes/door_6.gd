154e74765eaecf87843d90b5fae80e56b5edf979
pavlakertsadova <pavla.kertsadova@abv.bg>
2025-12-02 16:26:45 +0100

    wrati

---
+++ Scenes/door_6.gd
@@ -0,0 +1,14 @@
+extends Sprite2D
+
+@onready var area: Area2D = $Area2D
+
+func _ready():
+	area.body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body):
+	if body is Player:
+		_enter_level()
+
+func _enter_level():
+	var new_scene = load("res://scenes/puzzle6.tscn")
+	get_tree().change_scene_to_packed(new_scene)
