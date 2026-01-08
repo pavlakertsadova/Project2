8faeebdc19ec568bbeac8ed8623e65d45b67fc30
polidimitrova <polina.dimitrova@st.ovgu.de>
2026-01-08 17:58:13 +0100

    Update MainMenu.gd

--- Scenes/MainMenu.gd
+++ Scenes/MainMenu.gd
@@ -1,5 +1,7 @@
 extends Control
 
+func _ready():
+	AudioManager.set_mode(AudioManager.MusicMode.MENU)
 
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/node_2d.tscn")
